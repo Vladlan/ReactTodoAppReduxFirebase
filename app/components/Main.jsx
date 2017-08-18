1594bb09dc9a5b9b6f4f33a7b157dcfb9adba6c1
Vladlan <lanvlad911@gmail.com>
2017-08-18 22:00:49 +0300

    Second commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+//#2
+var Main = (props) => {
+    return (
+        <div>
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    Main.jsx
+                </div>
+            </div>
+        </div>
+    );
+};
+
+module.exports = Main;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx',
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+            Main: 'app/components/Main.jsx',
+            Countdown: 'app/components/Countdown.jsx',
+            Clock: 'app/components/Clock.jsx',
+            About: 'app/components/About.jsx',
+            CountdownForm: 'app/components/CountdownForm.jsx',
+            ErrorModal: 'app/components/ErrorModal.jsx',
+            Examples: 'app/components/Examples.jsx',
+            Controls: 'app/components/Controls.jsx',
+            applicationStyles: 'app/styles/app.scss'
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015', 'stage-2']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map'
+};
