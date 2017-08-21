bacc478204950bb4dde14631cf9d8f547d2e9d25
Vladlan <lanvlad911@gmail.com>
2017-08-21 17:06:39 +0300

    Added test to SearchTodoForm

---
+++ app/tests/components/SearchTodoForm.test.jsx
@@ -0,0 +1,34 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+
+var SearchTodoForm = require('SearchTodoForm');
+
+describe('SearchTodoForm', () => {
+    it('should exist', ()=> {
+        expect(SearchTodoForm).toExist();
+    });
+    it('should call onSearch with entered input text', () => {
+        var spy = expect.createSpy();
+        var searchTodoForm = TestUtils.renderIntoDocument(<SearchTodoForm onSearch={spy}/>);
+
+        searchTodoForm.refs.searchText.value = 'Finish app';
+        TestUtils.Simulate.change(searchTodoForm.refs.searchText);
+
+        expect(spy).toHaveBeenCalledWith(false, 'Finish app');
+    });
+
+    it('should call onSearch with proper check value', () => {
+        var spy = expect.createSpy();
+        var searchTodoForm = TestUtils.renderIntoDocument(<SearchTodoForm onSearch={spy}/>);
+
+        searchTodoForm.refs.showCompleted.checked = true;
+        searchTodoForm.refs.searchText.value = 'Finish app';
+        TestUtils.Simulate.change(searchTodoForm.refs.showCompleted);
+
+        expect(spy).toHaveBeenCalledWith(true, 'Finish app');
+    });
+
+});
